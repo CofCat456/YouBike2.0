ed875e3a34d4fbc618ad2220408052ef61b7a23c
CofCat456 <a25690756@gmail.com>
2023-12-25 22:37:47 +0800

    chore: setup eslint & prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: true,
+  plugins: [
+    require.resolve('prettier-plugin-tailwindcss'),
+  ],
+};
+
+module.exports = config;
